83ff96d507bf714057d54751604f5002f798abfa
Sreenath A S <srnths886@gmail.com>
2022-06-09 12:50:29 +0530

    Bugfix on table created at wrong database

--- app.js
+++ app.js
@@ -22,18 +22,13 @@ var pool = new Pool(credentials);
 let check_db = async () => {
     const result = await pool.query(`SELECT datname FROM pg_catalog.pg_database WHERE 
         lower(datname) = lower('phone-directory')`);
+
+    credentials.database = "phone-directory";
+
     if (result.rowCount == 0) {
         
-        await pool.query(`CREATE DATABASE "phone-directory"
-            WITH
-            OWNER = postgres
-            ENCODING = 'UTF8'
-            LC_COLLATE = 'English_United States.1252'
-            LC_CTYPE = 'English_United States.1252'
-            TABLESPACE = pg_default
-            CONNECTION LIMIT = -1;`);
+        await pool.query(`CREATE DATABASE "phone-directory"`);
 
-        credentials.database = "phone-directory";
         pool = new Pool(credentials);
 
         await pool.query(`CREATE TABLE directory
